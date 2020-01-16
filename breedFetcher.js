1bd5dd72bf6895c191e46eeaf77510e9232f18da
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-16 20:52:18 +0000

    Created unit tests for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,24 @@
 const request = require('request');
 
-const breedFetcher = () => {
-  const argument = process.argv.slice(2).join('');
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${argument}`, (error,response,body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error,response,body) => {
     
     if (error) {
-      console.log("Error, please verify your details and try again");
-      return "Error, please verify your details and try again";
+      //console.log("Error, please verify your details and try again");
+      callback("Error, please verify your details and try again", null);
     } else {
       const catObject = JSON.parse(body);
 
       if (catObject.length === 0) {
-        console.log("Breed Not Found");
-        return "Breed Not Found";
+        //console.log("Breed Not Found");
+        callback("Breed Not Found", null);
       } else {
-        console.log(catObject[0]['description']);
-        return catObject[0]['description'];
+        //console.log(catObject[0]['description']);
+        callback(null,catObject[0]['description']);
       }
 
     }
   });
 };
-breedFetcher();
+
+module.exports = {fetchBreedDescription};
